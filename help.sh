04ba4cda2515604854a08a3cb285f6c4387dc59c
Natalia49340 <129343279+Natalia49340@users.noreply.github.com>
2023-04-28 19:10:05 +0200

    update help.sh

--- help.sh
+++ help.sh
@@ -5,6 +5,10 @@ elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
 echo "Logi"
 elif [[ "$1" == "--date" || "$1" == "-d" ]]; then
 echo "Data"
+elif [[ "$1" == "--init" || "$1" == "i" ]]; then
+echo "Klonowanie"
+elif [[ "$1" == "--error" || "$1" == "-e" ]]; then
+echo "utworzenie 100 plików"
 else
 echo "Nieznana opcja"
 fi
@@ -13,3 +17,5 @@ echo "Dostępne opcje:"
 echo "--help, -h - wyświetlenie pomocy"
 echo "--logs, -l - wyświetlenie logów"
 echo "--date, -d - wyświetlenie daty"
+echo "--init - klonuje całe repozytorium do katalogu w którym został uruchomiony oraz ustawia ścieżkę w zmiennej środowiskowej PATH"
+echo "--error [n], -e [n] - tworzy n plików errorx/errorx.txt, gdzie x to numer błędu. Domyślnie tworzy 100 plików."
